da41fb485909bb28a4251e698fd7349f6a30d14e
Mike Bright <github.com@mjbright.net>
2018-03-22 15:13:26 +0100

    More complete, more checking

--- Orchestration-Kubernetes/to_slides_html.sh
+++ Orchestration-Kubernetes/to_slides_html.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-cd /root/ContainerOrchestration.Labs/Orchestration-Kubernetes
-mkdir -p html
+DIR="/root/ContainerOrchestration.Labs/Orchestration-Kubernetes"
+DIR="/home/mjb/Dropbox/z/bin/DEMOS_TUTORIALS/2018-Mar-KubeTutorial/Orchestration-Kubernetes"
+
+SERVING_DIR="/home/mjb/src/git/GIT_mjbright/Talks/kubetuto.html/"
+
+die() {
+    echo "$0: die - $*" >&2
+    exit 1
+}
+
+[ ! -d $DIR ]         && die "No such src dir <$DIR>"
+[ ! -d $SERVING_DIR ] && die "No such serving dir <$SERVING_DIR>"
+
+cd $DIR/
+
+mkdir -p html/images
 
 for NB in Kubernetes.ipynb 1.Concepts.ipynb 2.kubectl_basics.ipynb 3.pods.ipynb 4.* 5.maintenance.ipynb 6.tools.ipynb 9.Resources.ipynb ; do
     jupyter-nbconvert --to slides $NB
@@ -10,3 +24,4 @@ done
 mv *.html html/
 rsync -av images/ html/images/
 
+rsync -av html/ $SERVING_DIR/
