533154f219b2aae9910cc1b10b0309c5b2e6145a
Michael Bright <1880109+mjbright@users.noreply.github.com>
2018-11-07 05:56:24 +0100

    Added PS1 prompt

--- Orchestration-DockerSwarm/TPs/WinDockerVMShell.cmd
+++ Orchestration-DockerSwarm/TPs/WinDockerVMShell.cmd
@@ -1,4 +1,4 @@
 
-docker run --privileged --pid=host -it alpine:3.8 nsenter -t 1 -m -u -n -i -- sh -c "export LD_LIBRARY_PATH=/containers/services/docker-ce/lower/usr/lib; export PATH=/containers/services/docker-ce/lower/usr/local/bin:/containers/services/docker-ce/lower/usr/bin:$PATH; sh"
+docker run --privileged --pid=host -it alpine:3.8 nsenter -t 1 -m -u -n -i -- sh -c "export PS1='MobyLinuxVM[nsenter] \u@\h \w> '; export LD_LIBRARY_PATH=/containers/services/docker-ce/lower/usr/lib; export PATH=/containers/services/docker-ce/lower/usr/local/bin:/containers/services/docker-ce/lower/usr/bin:$PATH; sh"
 
 
